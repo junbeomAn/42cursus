6c3e2b1b8603cedc61439ffafe14ebc618b371f0
junbeoman <wnsqja8744@gmail.com>
2020-09-29 20:29:25 +0900

    memmove

---
+++ libft/ft_memmove.c
@@ -0,0 +1,29 @@
+#include "libft.h"
+
+void *ft_memmove(void *dest, const void *src, size_t n)
+{
+  size_t i;
+  unsigned char *dest_tmp;
+  unsigned char *src_tmp;
+  
+  dest_tmp = (unsigned char *)dest;
+  src_tmp = (unsigned char *)src;
+
+  if (dest == src)
+    return (dest);
+  i = 0;
+  if (dest < src)
+  {
+    while (i++ < n)
+      *dest_tmp++ = *src_tmp++;
+  }
+  else
+  {
+    src_tmp = src + (n - 1);
+    dest_tmp = dest + (n - 1);
+    while (i-- <= n)
+      *dest_tmp-- = *src_tmp--;
+  }
+  
+  return (dest);
+}
