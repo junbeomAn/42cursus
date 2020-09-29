5999edfb0dd0d3e9540f44eb7141686d12d63419
junbeoman <wnsqja8744@gmail.com>
2020-09-29 17:44:14 +0900

    bzero, memcpy, memccpy

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -1,16 +1,17 @@
 #include "libft.h"
 
-void *ft_memset(void *s, int c, size_t n)
+void ft_bzero(void *s, size_t n)
 {
   size_t i;
   unsigned char *temp;
+  unsigned char val;
 
   i = 0;
+  val = 0;
   temp = s;
   while (i++ < n)
   {
-    *temp = (unsigned char)c;
+    *temp = val;
     temp++;
   }
-  return (s);
 }
---
+++ libft/ft_memcpy.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+void *ft_memcpy(void *dest, const void *src, size_t n)
+{
+  size_t i;
+  unsigned char *temp;
+
+  i = 0;
+  temp = dest;
+  while (i++ < n)
+  {
+    *temp = *((const unsigned char *)src);
+    temp++;
+    src++;
+  }
+  return (dest);
+}
