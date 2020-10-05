be82b3486784965ba7402f4894c28c1b895a63d6
junbeoman <wnsqja8744@gmail.com>
2020-10-06 00:03:27 +0900

    strlcpy modified, substr

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -5,10 +5,9 @@ size_t ft_strlcpy(char *dst, const char *src, size_t size)
   size_t i;
 
   i = 0;
-  while (i < size - 1)
+  while (src[i] && i < size - 1)
   {
-    if (src[i])
-      dst[i] = src[i];
+    dst[i] = src[i];
     i++;
   }
   if (size > 0)
