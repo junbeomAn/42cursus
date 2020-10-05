70ed5e0014442585430a2c4ca831079fde545dce
junbeoman <wnsqja8744@gmail.com>
2020-10-05 21:42:51 +0900

    remove stdio header

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -1,7 +1,5 @@
 #include "libft.h"
 
-#include <stdio.h>
-
 void *ft_memcpy(void *dest, const void *src, size_t n)
 {
   size_t i;
--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -1,5 +1,5 @@
 #include "libft.h"
-#include <stdio.h>
+
 char *ft_strdup(const char *s)
 {
   void *dup;
