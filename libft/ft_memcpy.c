93fca1f681bbebc8614553522b05a0539cc0aba6
Junbeom An <juan@c2r1s8.42seoul.kr>
2020-10-10 22:14:02 +0900

    memcpy, substr

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -5,6 +5,9 @@ void  *ft_memcpy(void *dest, const void *src, size_t n)
   size_t        i;
   unsigned char *temp;
 
+  if (!dest && !src)
+	  return (0);
+
   i = 0;
   temp = dest;
   while (i < n)
--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -5,7 +5,7 @@ char  *ft_substr(char const *s, unsigned int start, size_t len)
   char  *ret;
 
   if (ft_strlen(s) < start)
-    return ('\0');
+    return (NULL);
   if (!(ret = (char *)malloc(sizeof(char) * (len + 1))))
     return (NULL);
   ft_memcpy((void *)ret, (const void*)(s + start), len);
