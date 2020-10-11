9c57893942c7b3fe286ace34c90be3274c6d2037
Junbeom An <juan@c2r1s8.42seoul.kr>
2020-10-11 16:33:31 +0900

    lstmap bug fixed(del null protection is not allowed. don't know why....

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -1,25 +1,34 @@
 #include "libft.h"
 
 t_list  *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
-{
-    t_list  *new_lst;
-    t_list  *temp;
-    void    *new_content;
 
-    if (!lst || !f || !del)
-        return (NULL);
-    new_lst = NULL;
-    while (lst)
-    {
-        new_content = f(lst->content);
-        if (!(temp = ft_lstnew(new_content)))
-        {
-            free(temp);
-            ft_lstclear(&new_lst, del);
-            return (NULL);
-        }            
-        ft_lstadd_back(&new_lst, temp);
-        lst = lst->next;
-    }
-    return (new_lst);
+{
+	t_list  *head;
+	t_list  *new_lst;
+	t_list  *temp;
+	void    *new_content;
+
+	if (!lst || !f)
+		return (NULL);
+	if (!(new_lst = ft_lstnew(f(lst->content))))
+	{
+		free(new_lst);
+		return (NULL);
+	}
+	head = new_lst;
+	lst = lst->next;
+	while (lst)
+	{
+		new_content = f(lst->content);
+		if (!(temp = ft_lstnew(new_content)))
+		{
+			free(temp);
+			ft_lstclear(&new_lst, del);
+			return (NULL);
+		}            
+		new_lst->next = temp;
+		new_lst = new_lst->next;
+		lst = lst->next;
+	}
+	return (head);
 }
