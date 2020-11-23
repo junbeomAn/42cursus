2411c7713e4024113894d05e98bd5c93ddd96a68
Junbeom An <junbeoman@Junbeomui-Macmini.local>
2020-11-23 22:33:50 +0900

    ftprintf p bug fixed

--- printf/conversion/handle_pointer.c
+++ printf/conversion/handle_pointer.c
@@ -6,7 +6,7 @@
 /*   By: junbeoman <junbeoman@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 11:25:02 by junbeoman         #+#    #+#             */
-/*   Updated: 2020/11/23 12:53:23 by junbeoman        ###   ########.fr       */
+/*   Updated: 2020/11/23 22:30:18 by junbeoman        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ static int	view_l_align(t_fmtstr *t, intptr_t p, int print_len)
 
 	ret = 0;
 	ret += ft_putstr("0x");
-	ret += ft_putnbr_base(p, HEXBASE);
+	if (p != 0 || t->precision != 0)
+		ret += ft_putnbr_base(p, HEXBASE);
 	ret += ft_put_blank(t->width - print_len, 0);
 	return (ret);
 }
@@ -30,7 +31,8 @@ static int	view_r_align(t_fmtstr *t, intptr_t p, int print_len)
 	ret = 0;
 	ret += ft_put_blank(t->width - print_len, 0);
 	ret += ft_putstr("0x");
-	ret += ft_putnbr_base(p, HEXBASE);
+	if (p != 0 || t->precision != 0)
+		ret += ft_putnbr_base(p, HEXBASE);
 	return (ret);
 }
 
@@ -41,6 +43,8 @@ int			handle_pointer(va_list vl, t_fmtstr *t, int ret)
 
 	p = (intptr_t)va_arg(vl, void *);
 	print_len = get_address_len(p, 16) + 2;
+	if (p == 0 && t->precision == 0)
+		print_len -= 1;
 	if (t->width > print_len)
 	{
 		if (is_left_aligned(t))
